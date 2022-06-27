2f2ac26443cb16e4bab900c8e07c0681b66bc615
shirokuma-89 <code@shirokuma89.dev>
2022-06-27 12:08:32 +0900

    判別のやつ

--- main.c
+++ main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
-    int numberA, numberB, numberC;
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &numberA, &numberB, &numberC);
+#include <math.h>
 
-    if(numberA <= numberB && numberB <= numberC) {
-        puts("OK");
+int main(void) {
+    int a, b, c;
+
+    printf("aを入力:");
+    scanf("%d", &a);
+
+    printf("bを入力:");
+    scanf("%d", &b);
+
+    printf("cを入力:");
+    scanf("%d", &c);
+
+    int judge = b * b - 4 * a * c;
+
+    if (judge > 0) {
+        printf("2つの実数解が存在する\n");
+    } else if (judge == 0) {
+        printf("重解が存在する\n");
     } else {
-        puts("NG");
+        printf("2つの虚数解が存在する\n");
     }
 
     return 0;
