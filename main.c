0dd22cf3a4cfe8ac5e86cb12b9c3081a8ae51880
shirokuma-89 <code@shirokuma89.dev>
2022-07-04 12:36:55 +0900

    課題3

--- main.c
+++ main.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-    int inputA, inputB;
+    int input;
 
-    puts("二つの整数を入力してください");
-    printf("A: ");
-    scanf("%d", &inputA);
-    printf("B: ");
-    scanf("%d", &inputB);
-
-    int min = (inputA < inputB) ? inputA : inputB;
-    int max = (inputA > inputB) ? inputA : inputB;
-
-    printf("%d以上%d以下の全て整数の和は", min, max);
-
-    int sum = 0;
-    while (min != max + 1) {
-        sum += min++;
+    printf("正の整数を入力してください: ");
+    scanf("%d", &input);
+    
+    if (input > 0) {
+        while (input >= 0) {
+            printf("%d", input);
+            input--;
+        }
+        printf("\n");
     }
-    printf("%dです\n", sum);
 
     return 0;
 }
