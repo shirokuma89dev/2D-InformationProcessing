6525f01df499965d7bae61899e7654fac45f6d4f
shirokuma-89 <code@shirokuma89.dev>
2022-07-04 11:56:53 +0900

    課題1

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-    int input;
+    int shoudRetry;
+    
+    do {
+        int input;
 
-    printf("正の整数を入力してください: ");
-    scanf("%d", &input);
+        printf("整数を入力してください: ");
+        scanf("%d", &input);
 
-    int i = 0;
-    while (i <= input) {
-        printf("%d", i++);
-    }
-
-    printf("\n");
+        if (input == 0) {
+            puts("その数は0です。");
+        } else if (input > 0) {
+            puts("その数は正です。");
+        } else {
+            puts("その数は負です。");
+        }
+        printf("もう一度やりますか? (1:はい, 0:いいえ): ");
+        scanf("%d", &shoudRetry);
+    } while (shoudRetry == 1);
     return 0;
 }
