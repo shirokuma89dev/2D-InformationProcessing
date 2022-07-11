306d6b9aeff8dcbfb5eeab122f8240848c262b63
shirokuma-89 <code@shirokuma89.dev>
2022-07-11 23:22:33 +0900

    演習4-6

--- main.c
+++ main.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-    puts("正の整数値を加算します。終了するには-9999を入力：");
-    int sum = 0;
-    while (1) {
-        int input;
-        printf("整数値：");
-        scanf("%d", &input);
+    printf("正の整数を入力せよ：");
 
-        if (input == -9999) {
-            break;
-        } else if (input < 0) {
-            printf("負の整数値は加算できません。\n");
-            continue;
-        }
+    int input;
+    scanf("%d", &input);
 
-        sum += input;
+    int counter = 2;
+    while (counter < input) {
+        printf("%d ", counter);
+        counter += 2;
     }
-
-    puts("-------------------------");
-    printf("加算結果：%d\n", sum);
 
     return 0;
 }
